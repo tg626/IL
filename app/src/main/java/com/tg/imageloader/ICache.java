32f86d75528b8ab1b87355ecad12672b19f984dc
tg <tg626@qq.com>
2016-02-11 21:18:25 +0800

    modify ICache interface

--- app/src/main/java/com/tg/imageloader/ICache.java
+++ app/src/main/java/com/tg/imageloader/ICache.java
@@ -1,9 +1,17 @@
 package com.tg.imageloader;
 
 /**
+ * 缓存协议
  * Created by tg on 2016/2/11.
  */
 public interface ICache {
+    /*
+        读取缓存
+    */
     public void getCache();
+
+    /*
+        保存缓存
+     */
     public void setCache();
 }
